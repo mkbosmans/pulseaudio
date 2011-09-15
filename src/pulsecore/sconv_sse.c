43ec2fc31f54b1ac8a204989ae02c51b67f684d8
Maarten Bosmans <mkbosmans@gmail.com>
2011-09-15 15:40:37 +0200

    sconv: Use SSE optimized conversion where applicable

--- src/pulsecore/sconv_sse.c
+++ src/pulsecore/sconv_sse.c
@@ -167,9 +167,11 @@ void pa_convert_func_init_sse(pa_cpu_x86_flag_t flags) {
     if (flags & PA_CPU_X86_SSE2) {
         pa_log_info("Initialising SSE2 optimized conversions.");
         pa_set_convert_from_float32ne_function(PA_SAMPLE_S16LE, (pa_convert_func_t) sconv_s16le_from_f32ne_sse2);
+        pa_set_convert_to_s16ne_function(PA_SAMPLE_FLOAT32NE, (pa_convert_func_t) sconv_s16le_from_f32ne_sse2);
     } else {
         pa_log_info("Initialising SSE optimized conversions.");
         pa_set_convert_from_float32ne_function(PA_SAMPLE_S16LE, (pa_convert_func_t) sconv_s16le_from_f32ne_sse);
+        pa_set_convert_to_s16ne_function(PA_SAMPLE_FLOAT32NE, (pa_convert_func_t) sconv_s16le_from_f32ne_sse);
     }
 
 #endif /* defined (__i386__) || defined (__amd64__) */
